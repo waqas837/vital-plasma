43d488d2bc800f0a84c31463ac6cbb2ea71c7a13
waqas837 <waqaskhanbughlani1124@gmail.com>
2025-05-16 12:42:55 +0500

    ok

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require("url");
 const next = require("next");
 
  
-const port = parseInt(process.env.PORT || "3001", 10);
+const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
